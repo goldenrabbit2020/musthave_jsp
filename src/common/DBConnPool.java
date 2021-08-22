0a4c9b88fea6c1d5cd997c5b6a95aa6772019c11
goldenrabbit Co., Ltd <82482667+goldenrabbit2020@users.noreply.github.com>
2021-08-22 21:17:02 +0900

    Update DBConnPool.java

--- src/common/DBConnPool.java
+++ src/common/DBConnPool.java
@@ -14,17 +14,17 @@ public class DBConnPool {
     public Statement stmt;
     public PreparedStatement psmt;
     public ResultSet rs;
-        
+
     // 기본 생성자
     public DBConnPool() {
         try {
             // 커넥션 풀(DataSource) 얻기
             Context initCtx = new InitialContext();
             Context ctx = (Context)initCtx.lookup("java:comp/env");
-            DataSource source = (DataSource)ctx.lookup("dbcp_myoracle");  
+            DataSource source = (DataSource)ctx.lookup("dbcp_myoracle");
 
             // 커넥션 풀을 통해 연결 얻기
-            con = source.getConnection(); 
+            con = source.getConnection();
 
             System.out.println("DB 커넥션 풀 연결 성공");
         }
@@ -33,14 +33,14 @@ public class DBConnPool {
             e.printStackTrace();
         }
     }
-    
+
     // 연결 해제(자원 반납)
     public void close() {
         try {            
             if (rs != null) rs.close();
             if (stmt != null) stmt.close();
             if (psmt != null) psmt.close();
-            if (con != null) con.close();  // 자동으로 커넥션 풀로 반납됨 ❺ 
+            if (con != null) con.close();  // 자동으로 커넥션 풀로 반납됨
 
             System.out.println("DB 커넥션 풀 자원 반납");
         }
