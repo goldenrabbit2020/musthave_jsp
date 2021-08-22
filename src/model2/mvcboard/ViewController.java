071470a55e2712037190f8591dcf0a85b2e5eb34
goldenrabbit Co., Ltd <82482667+goldenrabbit2020@users.noreply.github.com>
2021-08-22 21:41:06 +0900

    Update ViewController.java

--- src/model2/mvcboard/ViewController.java
+++ src/model2/mvcboard/ViewController.java
@@ -8,19 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/mvcboard/view.do")
-public class ViewController extends HttpServlet {    
+public class ViewController extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) 
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
         throws ServletException, IOException {
         // 게시물 불러오기
         MVCBoardDAO dao = new MVCBoardDAO();
         String idx = req.getParameter("idx");
         dao.updateVisitCount(idx);  // 조회수 1 증가
-        MVCBoardDTO dto = dao.selectView(idx);    
+        MVCBoardDTO dto = dao.selectView(idx);
         dao.close();
 
         // 줄바꿈 처리
-        dto.setContent(dto.getContent().replaceAll("\r\n", "<br/>"));  
+        dto.setContent(dto.getContent().replaceAll("\r\n", "<br/>"));
 
         // 게시물(dto) 저장 후 뷰로 포워드
         req.setAttribute("dto", dto);
