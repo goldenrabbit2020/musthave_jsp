71792a9f4e185d2237fcc63ddd822fa1b8c989e8
goldenrabbit Co., Ltd <82482667+goldenrabbit2020@users.noreply.github.com>
2021-08-22 21:26:45 +0900

    메서드명 수정

--- src/model2/mvcboard/WriteController.java
+++ src/model2/mvcboard/WriteController.java
@@ -18,24 +18,24 @@ import utils.JSFunction;
 
 public class WriteController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         req.getRequestDispatcher("/14MVCBoard/Write.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         // 1. 파일 업로드 처리 =============================
         // 업로드 디렉터리의 물리적 경로 확인
-        String saveDirectory = req.getServletContext().getRealPath("/Uploads");  
+        String saveDirectory = req.getServletContext().getRealPath("/Uploads");
 
         // 초기화 매개변수로 설정한 첨부 파일 최대 용량 확인
-        ServletContext application = getServletContext();  
+        ServletContext application = getServletContext();
         int maxPostSize = Integer.parseInt(application.getInitParameter("maxPostSize"));
 
         // 파일 업로드
-        MultipartRequest mr = FileUtil.upload(req, saveDirectory, maxPostSize);  
+        MultipartRequest mr = FileUtil.uploadFile(req, saveDirectory, maxPostSize);
         if (mr == null) {
             // 파일 업로드 실패
             JSFunction.alertLocation(resp, "첨부 파일이 제한 용량을 초과합니다.",
@@ -44,11 +44,11 @@ public class WriteController extends HttpServlet {
         }
 
         // 2. 파일 업로드 외 처리 =============================
-        // 폼값을 DTO에 저장 
+        // 폼값을 DTO에 저장
         MVCBoardDTO dto = new MVCBoardDTO(); 
         dto.setName(mr.getParameter("name"));
         dto.setTitle(mr.getParameter("title"));
-        dto.setContent(mr.getParameter("content"));            
+        dto.setContent(mr.getParameter("content"));
         dto.setPass(mr.getParameter("pass"));
 
         // 원본 파일명과 저장된 파일 이름 설정
@@ -68,9 +68,9 @@ public class WriteController extends HttpServlet {
             dto.setOfile(fileName);  // 원래 파일 이름
             dto.setSfile(newFileName);  // 서버에 저장된 파일 이름
         }
-        
+
         // DAO를 통해 DB에 게시 내용 저장
-        MVCBoardDAO dao = new MVCBoardDAO();       
+        MVCBoardDAO dao = new MVCBoardDAO();
         int result = dao.insertWrite(dto);
         dao.close();
 
