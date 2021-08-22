a3199ea9ba68716b8999ab442b630879b78a17df
goldenrabbit Co., Ltd <82482667+goldenrabbit2020@users.noreply.github.com>
2021-08-22 21:40:49 +0900

    Update PassController.java

--- src/model2/mvcboard/PassController.java
+++ src/model2/mvcboard/PassController.java
@@ -13,19 +13,19 @@ import fileupload.FileUtil;
 import utils.JSFunction;
 
 @WebServlet("/mvcboard/pass.do")
-public class PassController extends HttpServlet {    
+public class PassController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
-        throws ServletException, IOException {        
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
         req.setAttribute("mode", req.getParameter("mode"));
         req.getRequestDispatcher("/14MVCBoard/Pass.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         // 매개변수 저장
-        String idx = req.getParameter("idx");  
+        String idx = req.getParameter("idx");
         String mode = req.getParameter("mode");
         String pass = req.getParameter("pass");
 
@@ -33,7 +33,7 @@ public class PassController extends HttpServlet {
         MVCBoardDAO dao = new MVCBoardDAO();
         boolean confirmed = dao.confirmPassword(pass, idx);
         dao.close();
-        
+
         if (confirmed) {  // 비밀번호 일치
             if (mode.equals("edit")) {  // 수정 모드
                 HttpSession session = req.getSession();
@@ -43,7 +43,7 @@ public class PassController extends HttpServlet {
             else if (mode.equals("delete")) {  // 삭제 모드
                 dao = new MVCBoardDAO();
                 MVCBoardDTO dto = dao.selectView(idx);
-                int result = dao.deletePost(idx);  // 게시물 삭제 
+                int result = dao.deletePost(idx);  // 게시물 삭제
                 dao.close();
                 if (result == 1) {  // 게시물 삭제 성공 시 첨부파일도 삭제
                     String saveFileName = dto.getSfile();
