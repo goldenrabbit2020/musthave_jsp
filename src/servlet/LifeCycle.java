90eef29f75dafb4aac00430d5e28ff155892213d
goldenrabbit Co., Ltd <82482667+goldenrabbit2020@users.noreply.github.com>
2021-08-22 21:43:19 +0900

    Update LifeCycle.java

--- src/servlet/LifeCycle.java
+++ src/servlet/LifeCycle.java
@@ -17,38 +17,38 @@ public class LifeCycle extends HttpServlet {
     public void myPostConstruct() {
         System.out.println("myPostConstruct() 호출");
     }
-    
+
     @Override
     public void init() throws ServletException {
         System.out.println("init() 호출");
     }
-    
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         System.out.println("service() 호출");
         super.service(req, resp);
     }        
-    
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
             throws ServletException, IOException {
         System.out.println("doGet() 호출");
         req.getRequestDispatcher("/13Servlet/LifeCycle.jsp").forward(req, resp);
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
             throws ServletException, IOException {
         System.out.println("doPost() 호출");
         req.getRequestDispatcher("/13Servlet/LifeCycle.jsp").forward(req, resp);
     }
-    
+
     @Override
     public void destroy() {
         System.out.println("destroy() 호출");
     }
-       
+
     @PreDestroy
     public void myPreDestroy() {
         System.out.println("myPreDestroy() 호출");
