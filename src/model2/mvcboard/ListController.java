8a8d9292305b619265287aaa201e403de43fb700
goldenrabbit Co., Ltd <82482667+goldenrabbit2020@users.noreply.github.com>
2021-08-22 21:37:48 +0900

    Update ListController.java

--- src/model2/mvcboard/ListController.java
+++ src/model2/mvcboard/ListController.java
@@ -15,13 +15,13 @@ import utils.BoardPage;
 
 public class ListController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
        // DAO 생성
-       MVCBoardDAO dao = new MVCBoardDAO();   
+       MVCBoardDAO dao = new MVCBoardDAO();
 
         // 뷰에 전달할 매개변수 저장용 맵 생성
-        Map<String, Object> map = new HashMap<String, Object>();   
+        Map<String, Object> map = new HashMap<String, Object>();
 
         String searchField = req.getParameter("searchField");
         String searchWord = req.getParameter("searchWord");
@@ -34,8 +34,8 @@ public class ListController extends HttpServlet {
 
         /* 페이지 처리 start */
         ServletContext application = getServletContext();
-        int pageSize = Integer.parseInt(application.getInitParameter("POSTS_PER_PAGE"));  // 페이지당 게시물 수
-        int blockPage = Integer.parseInt(application.getInitParameter("PAGES_PER_BLOCK"));  // 블록당 페이지 수
+        int pageSize = Integer.parseInt(application.getInitParameter("POSTS_PER_PAGE"));
+        int blockPage = Integer.parseInt(application.getInitParameter("PAGES_PER_BLOCK"));
 
         // 현재 페이지 확인
         int pageNum = 1;  // 기본값
@@ -52,15 +52,15 @@ public class ListController extends HttpServlet {
 
         List<MVCBoardDTO> boardLists = dao.selectListPage(map);  // 게시물 목록 받기
         dao.close(); // DB 연결 닫기
-        
+
         // 뷰에 전달할 매개변수 추가
         String pagingImg = BoardPage.pagingStr(totalCount, pageSize,
                 blockPage, pageNum, "../mvcboard/list.do");  // 바로가기 영역 HTML 문자열
-        map.put("pagingImg", pagingImg);        
-        map.put("totalCount", totalCount);        
+        map.put("pagingImg", pagingImg);
+        map.put("totalCount", totalCount);
         map.put("pageSize", pageSize);
         map.put("pageNum", pageNum);
-        
+
         // 전달할 데이터를 request 영역에 저장 후 List.jsp로 포워드
         req.setAttribute("boardLists", boardLists);
         req.setAttribute("map", map);
