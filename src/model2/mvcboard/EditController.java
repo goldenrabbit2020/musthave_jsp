121f082e15cfbc49dcff3c564cea79b0ef994235
goldenrabbit Co., Ltd <82482667+goldenrabbit2020@users.noreply.github.com>
2021-08-22 21:25:03 +0900

    메서드명 수정

--- src/model2/mvcboard/EditController.java
+++ src/model2/mvcboard/EditController.java
@@ -19,9 +19,9 @@ import fileupload.FileUtil;
 import utils.JSFunction;
 
 @WebServlet("/mvcboard/edit.do")
-public class EditController extends HttpServlet {    
+public class EditController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String idx = req.getParameter("idx");
         MVCBoardDAO dao = new MVCBoardDAO();
@@ -31,18 +31,18 @@ public class EditController extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
         throws ServletException, IOException {
         // 1. 파일 업로드 처리 =============================
         // 업로드 디렉터리의 물리적 경로 확인
-        String saveDirectory = req.getServletContext().getRealPath("/Uploads"); 
+        String saveDirectory = req.getServletContext().getRealPath("/Uploads");
 
         // 초기화 매개변수로 설정한 첨부 파일 최대 용량 확인
-        ServletContext application = this.getServletContext();  
+        ServletContext application = this.getServletContext();
         int maxPostSize = Integer.parseInt(application.getInitParameter("maxPostSize"));
 
         // 파일 업로드
-        MultipartRequest mr = FileUtil.upload(req, saveDirectory, maxPostSize);
+        MultipartRequest mr = FileUtil.uploadFile(req, saveDirectory, maxPostSize);
 
         if (mr == null) {
             // 파일 업로드 실패
@@ -55,39 +55,39 @@ public class EditController extends HttpServlet {
         String idx = mr.getParameter("idx");
         String prevOfile = mr.getParameter("prevOfile");
         String prevSfile = mr.getParameter("prevSfile");
-                        
+
         String name = mr.getParameter("name");
         String title = mr.getParameter("title");
         String content = mr.getParameter("content");
             
         // 비밀번호는 session에서 가져옴
-        HttpSession session = req.getSession(); 
+        HttpSession session = req.getSession();
         String pass = (String)session.getAttribute("pass");
 
         // DTO에 저장
-        MVCBoardDTO dto = new MVCBoardDTO(); 
+        MVCBoardDTO dto = new MVCBoardDTO();
         dto.setIdx(idx);
         dto.setName(name);
         dto.setTitle(title);
-        dto.setContent(content);            
+        dto.setContent(content);
         dto.setPass(pass);
             
         // 원본 파일명과 저장된 파일 이름 설정
-        String fileName = mr.getFilesystemName("ofile");  
+        String fileName = mr.getFilesystemName("ofile");
         if (fileName != null) {
-            // 첨부 파일이 있을 경우 파일명 변경 
+            // 첨부 파일이 있을 경우 파일명 변경
             // 새로운 파일명 생성
             String now = new SimpleDateFormat("yyyyMMdd_HmsS").format(new Date());
             String ext = fileName.substring(fileName.lastIndexOf("."));
             String newFileName = now + ext;
-                
+
             // 파일명 변경
             File oldFile = new File(saveDirectory + File.separator + fileName);
             File newFile = new File(saveDirectory + File.separator + newFileName);
             oldFile.renameTo(newFile);
-            
+
             // DTO에 저장
-            dto.setOfile(fileName);  // 원래 파일 이름 
+            dto.setOfile(fileName);  // 원래 파일 이름
             dto.setSfile(newFileName);  // 서버에 저장된 파일 이름
 
             // 기존 파일 삭제
@@ -95,18 +95,18 @@ public class EditController extends HttpServlet {
         }
         else {
             // 첨부 파일이 없으면 기존 이름 유지
-            dto.setOfile(prevOfile);  
+            dto.setOfile(prevOfile);
             dto.setSfile(prevSfile);
         }
-        
+
         // DB에 수정 내용 반영
-        MVCBoardDAO dao = new MVCBoardDAO();            
+        MVCBoardDAO dao = new MVCBoardDAO();
         int result = dao.updatePost(dto);
         dao.close();
 
         // 성공 or 실패?
         if (result == 1) {  // 수정 성공
-            session.removeAttribute("pass"); 
+            session.removeAttribute("pass");
             resp.sendRedirect("../mvcboard/view.do?idx=" + idx);
         }
         else {  // 수정 실패
